80f00662fdcee11a3ee802abe5c0fdc05b6a4aa1
Prabesh <prabesh5511@gmail.com>
2025-05-27 11:04:08 +0545

    update main

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+// import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
@@ -27,9 +27,7 @@ const router = createBrowserRouter([
 
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
     <RouterProvider router={router}>
     <App />
     </RouterProvider>
-  </StrictMode>
 );
